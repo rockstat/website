8b462bf1e334b833617cc2f4b4f0ce4ca68e7122
Dmitry Rodin <madiedinro@gmail.com>
2018-08-22 10:28:32 +0300

    replace

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Router } from '../next-routes';
 
-
 export default class extends React.Component {
   static async getInitialProps({ res, query }) {
     if (!res) {
-      Router.pushRoute('/ru')
+      Router.replaceRoute('/ru')
     }
     return {}
   }
