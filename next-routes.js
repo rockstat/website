b034f52c7d388d3bb91a3e4af116fc0c6256aa08
Dmitry Rodin <madiedinro@gmail.com>
2018-08-28 17:31:45 +0300

    ru only

--- next-routes.js
+++ next-routes.js
@@ -4,7 +4,7 @@ const routes = require('next-routes')
 // routes.add([name], pattern = /name, page = name)
 module.exports = routes()
   .add('index', '/', 'index')
-  .add('main', '/:lang(en|ru)', 'main')
+  .add('main', '/ru', 'main')
   .add('docs', '/docs', 'docs')
   // .add('blog', '/:lang(en|ru)/blog/:slug')
 
